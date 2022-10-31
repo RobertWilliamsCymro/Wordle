4458946e45750abdfd3659feb0e171348e500be4
Robert Williams <robert.williams@jisc.ac.uk>
2022-10-31 17:16:05 +0000

    amended code for checking correct letters

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,51 @@
-const tileDisplay = document.querySelector("#tile-container") as HTMLDivElement;
-const keyboard = document.querySelector("#key-container") as HTMLDivElement;
+const tileDisplay = document.querySelector('#tile-container') as HTMLDivElement;
+const keyboard = document.querySelector('#key-container') as HTMLDivElement;
 const messageDisplay = document.querySelector(
-  "#message-container"
+  '#message-container'
 ) as HTMLDivElement;
 
 let currentRow = 0 as number;
 let currentTile = 0 as number;
 let isGameOver = false as boolean;
 
-const wordle = "super" as string;
+const wordle = 'super' as string;
 const keys = [
-  "Q",
-  "W",
-  "E",
-  "R",
-  "T",
-  "Y",
-  "U",
-  "I",
-  "O",
-  "P",
-  "A",
-  "S",
-  "D",
-  "F",
-  "G",
-  "H",
-  "J",
-  "K",
-  "L",
-  "Z",
-  "X",
-  "C",
-  "V",
-  "ENTER",
-  "B",
-  "N",
-  "M",
-  "<<",
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'ENTER',
+  'B',
+  'N',
+  'M',
+  '<<',
 ] as string[];
 
 const handleClick = (letter: string) => {
-  console.log("click", letter);
-  if (letter === "<<") {
+  if (letter === '<<') {
     deleteLetter();
     return;
   }
-  if (letter === "ENTER") {
+  if (letter === 'ENTER') {
     checkRow();
     return;
   }
@@ -60,9 +59,8 @@ const addLetter = (letter: string) => {
     ) as HTMLElement;
     tile.textContent = letter;
     guessRows[currentRow][currentTile] = letter;
-    tile.setAttribute("data", letter);
+    tile.setAttribute('data', letter);
     currentTile++;
-    console.log("guessRows", guessRows);
   }
 };
 
@@ -72,26 +70,26 @@ const deleteLetter = () => {
     const tile = document.getElementById(
       `guessRow-${currentRow}-tile-${currentTile}`
     ) as HTMLElement;
-    tile.textContent = "";
-    guessRows[currentRow][currentTile] = "";
-    tile.setAttribute("data", "");
+    tile.textContent = '';
+    guessRows[currentRow][currentTile] = '';
+    tile.setAttribute('data', '');
   }
 };
 
 const checkRow = () => {
   //below is a neat way of joining all individual values in array into one string
-  const guess = guessRows[currentRow].join("");
+  const guess = guessRows[currentRow].join('');
 
   if (currentTile > 4) {
     flipTile();
     if (wordle.toUpperCase() === guess) {
-      showMessage("Correct Answer!");
+      showMessage('Correct Answer!');
       isGameOver = true;
       return;
     } else {
       if (currentRow >= 5) {
         isGameOver = false;
-        showMessage("Game Over");
+        showMessage('Game Over');
         return;
       }
       if (currentRow <= 5) {
@@ -103,8 +101,8 @@ const checkRow = () => {
 };
 
 const showMessage = (message: string) => {
-  const messageElement = document.createElement("p");
-  messageElement.classList.value = "m-0 p-10 rounded-lg";
+  const messageElement = document.createElement('p');
+  messageElement.classList.value = 'm-0 p-10 rounded-lg';
   messageElement.textContent = message;
   //below code puts message into div tag
   messageDisplay.append(messageElement);
@@ -114,56 +112,80 @@ const showMessage = (message: string) => {
 
 //below is a
 const guessRows = [
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
 ] as string[][];
 
 guessRows.forEach((guessRow, guessRowIndex: number) => {
-  const rowElement = document.createElement("div") as HTMLDivElement;
-  rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
+  const rowElement = document.createElement('div') as HTMLDivElement;
+  rowElement.setAttribute('id', 'guessRow-' + guessRowIndex);
 
   guessRow.forEach((guess, guessIndex) => {
-    const tileElement = document.createElement("div");
+    const tileElement = document.createElement('div');
     tileElement.setAttribute(
-      "id",
-      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
+      'id',
+      'guessRow-' + guessRowIndex + '-tile-' + guessIndex
     );
 
     tileElement.classList.value =
-      "m-2 w-[62px] h-[62px] bg-slate-500 border-solid border-slate-500";
+      'm-2 w-[62px] h-[62px] bg-slate-500 border-solid border-slate-500';
     rowElement.append(tileElement);
   });
   tileDisplay.append(rowElement);
 });
 
 keys.forEach((key) => {
-  const buttonElement = document.createElement("button") as HTMLButtonElement;
+  const buttonElement = document.createElement('button') as HTMLButtonElement;
   buttonElement.classList.value =
-    "w-[43px] h-[58px] m-4 rounded border-none bg-gray-500 [&:nth-child(28)]:w-[80px] [&:nth-child(24)]:w-[80px]";
+    'w-[43px] h-[58px] m-4 rounded border-none bg-gray-500 [&:nth-child(28)]:w-[80px] [&:nth-child(24)]:w-[80px]';
   buttonElement.textContent = key;
-  buttonElement.setAttribute("id", key);
+  buttonElement.setAttribute('id', key);
   //callback function is needed here because we are using a parameter
   //without the () => the function will be called right away, have a look at asynch javascript to understand this better!
-  buttonElement.addEventListener("click", () => handleClick(key));
+  buttonElement.addEventListener('click', () => handleClick(key));
   keyboard.append(buttonElement);
 });
 
-const flipTile = () => {
-  const rowTiles = document.querySelector(`#guessRow-${currentRow}`)?.childNodes as NodeListOf<HTMLElement>;
-  
-  rowTiles?.forEach((tile: HTMLElement, index: number) => {
-    const dataLetter = tile.getAttribute("data") as string;
-
-    if (dataLetter.toLowerCase() == wordle[index])
-      tile.classList.value += " bg-green-700";
-    else if (wordle.includes(dataLetter))
-      tile.classList.value += " bg-yellow-900";
-    else tile.classList.value += " bg-red-700";
-  });
+const addColourToKey = (keyLetter: string, colour: string) => {
+  const key = document.getElementById(keyLetter) as HTMLElement;
+  key.classList.add(colour);
 };
 
+const flipTile = () => {
+  const rowTiles = document.querySelector(`#guessRow-${currentRow}`)
+    ?.childNodes as NodeListOf<HTMLElement>;
+  let checkWordle: string = wordle;
+  let guess: any[] = [];
+
+  // code below used to collect all data
+  rowTiles.forEach((tile) => {
+    guess.push({ letter: tile.getAttribute('data')?.toLowerCase(), colour: 'bg-red-700' });
+  });
+
+  guess.forEach((guess, index) => {
+
+    if (guess.letter  === wordle[index]) {
+      guess.colour = 'bg-green-700';
+      checkWordle = checkWordle.replace(guess.letter, '');
+    }
+  });
+
+  guess.forEach((guess) => {
+    if (checkWordle.includes(guess.letter)) {
+      guess.colour = 'bg-yellow-700';
+      checkWordle = checkWordle.replace(guess.letter, '');
+    }
+  });
+
+  rowTiles?.forEach((tile: HTMLElement, index: number) => {
+    setTimeout(() => {
+      tile.classList.value += ` ${guess[index].colour}`;
+      addColourToKey(guess[index].letter, guess[index].colour);
+    }, 500 * index);
+  });
+};
 export {};
